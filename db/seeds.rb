a1f07a776d6e6890ef48f3141c3f5db72291e831
Shane Nicholson <shanescottnicholson@gmail.com>
2021-11-02 14:26:52 -0700

    seeds speakers and meetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Speaker.create(first_name: "Alexander", last_name: "Rittenhouse", email: "arnie@example.com")
+# Speaker.create(first_name: "Bartholomew", last_name: "Acre", email: "bart@example.com")
+# Speaker.create(first_name: "Cillian", last_name: "Hempburn", email: "cecil@example.com")
+
+# Meeting.create(title: "Ted", agenda: "Global Cooling", location: "Berlin", time: "3:30 P.M.")
+# Meeting.create(title: "Ted-X", agenda: "Robots", location: "Boston", time: "10:00 A.M.")
